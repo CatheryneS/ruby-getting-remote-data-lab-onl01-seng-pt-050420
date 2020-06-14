12252700ebc04167f2549d2e2b4b68febe0abfe6
Catheryne Small <catheryn.small@gmail.com>
2020-06-14 04:28:11 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,10 @@ require 'net/http'
 require 'json'
 
 class GetRequester
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  attr_reader :url
+  def initialize(url)
+    @url = url
+  end
   
   def get_response_body
     
